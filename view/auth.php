efd8fc7d30820be9baee54e9dfef2560f5a56caa
Saymon <165163931+SaySubr@users.noreply.github.com>
2025-10-22 21:41:52 +0700

    first page
    add first pages

---
+++ view/auth.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Авторизация</title>
+</head>
+<body>
+    
+    <h1>Заглушка</h1>
+
+    <header>
+        <div class="header-content">
+            <div class="logo">Exam PHP</div>
+            <a href="welcome.php" class="login-button">На главную</a>
+        </div>
+    </header>
+
+     <div id="description"></div>
+    <footer>Студент: Семён.</footer>
+    </div>
+
+</body>
+</html>
+
